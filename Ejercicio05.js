78f13562e62ad753864b4cb9ea4473a7094ef35c
Tatiana Alejandra Jaramillo <talejandra.jaramillo@gmail.com>
2023-06-20 15:19:15 -0500

    (Feat)Ejercicio05: Dibujando una X con asteriscos

---
+++ Ejercicio05.js
@@ -0,0 +1,16 @@
+const drawX = (tamano) => {
+    for (let i = 0; i < tamano; i++) {
+      let linea = '';
+      for (let j = 0; j < tamano; j++) {
+        if (i === j || i === tamano - j - 1) {
+          linea += '* ';
+        } else {
+          linea += '  ';
+        }
+      }
+      console.log(linea);
+    }
+  }
+  
+/*--------------------Ejecutar el algoritmo---------------------*/
+  drawX(5);
