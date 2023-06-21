1806d4accaf8d298b3ae0bf7d64d82bcaac778c7
Tatiana Alejandra Jaramillo <talejandra.jaramillo@gmail.com>
2023-06-20 20:36:07 -0500

    Ejercicio01 Fixes

--- Ejercicio01.js
+++ Ejercicio01.js
@@ -1,24 +1,27 @@
 const next_pal = (numero) => {
-    while (!esPalindromo(numero)) {
-       numero++;
-     }
-       return numero;
-   };
- 
- const esPalindromo = (numero) => {
-     const numeroStr = String(numero);
-     const reversoStr = numeroStr.split('').reverse().join('');
-     return numeroStr === reversoStr;
- };
- 
- /*--------------------Ejecutar el algoritmo---------------------*/
- 
- let numero = 188;
- let siguientePalindromo = next_pal(numero);
- 
- console.log(
-     "El siguiente palíndromo a partir de",
-     numero,
-     "es",
-     siguientePalindromo
- );
+  if (numero < 0) {
+    throw new Error("El número debe ser positivo");
+  }
+  while (!esPalindromo(numero)) {
+      numero++;
+    }
+      return numero;
+  };
+
+const esPalindromo = (numero) => {
+    const numeroStr = String(numero);
+    const reversoStr = numeroStr.split('').reverse().join('');
+    return numeroStr === reversoStr;
+};
+
+/*--------------------Ejecutar el algoritmo---------------------*/
+
+let numero = 188;
+let siguientePalindromo = next_pal(numero);
+
+console.log(
+    "El siguiente palíndromo a partir de",
+    numero,
+    "es",
+    siguientePalindromo
+);
