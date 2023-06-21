7c9d7f434b434b2725e8d7e98083a784e089de4a
Tatiana Alejandra Jaramillo <talejandra.jaramillo@gmail.com>
2023-06-21 08:45:00 -0500

    Revert "Ejercicio01 Fixes"
    This reverts commit 1806d4accaf8d298b3ae0bf7d64d82bcaac778c7.

--- Ejercicio01.js
+++ Ejercicio01.js
@@ -1,27 +1,24 @@
 const next_pal = (numero) => {
-  if (numero < 0) {
-    throw new Error("El número debe ser positivo");
-  }
-  while (!esPalindromo(numero)) {
-      numero++;
-    }
-      return numero;
-  };
-
-const esPalindromo = (numero) => {
-    const numeroStr = String(numero);
-    const reversoStr = numeroStr.split('').reverse().join('');
-    return numeroStr === reversoStr;
-};
-
-/*--------------------Ejecutar el algoritmo---------------------*/
-
-let numero = 188;
-let siguientePalindromo = next_pal(numero);
-
-console.log(
-    "El siguiente palíndromo a partir de",
-    numero,
-    "es",
-    siguientePalindromo
-);
+    while (!esPalindromo(numero)) {
+       numero++;
+     }
+       return numero;
+   };
+ 
+ const esPalindromo = (numero) => {
+     const numeroStr = String(numero);
+     const reversoStr = numeroStr.split('').reverse().join('');
+     return numeroStr === reversoStr;
+ };
+ 
+ /*--------------------Ejecutar el algoritmo---------------------*/
+ 
+ let numero = 188;
+ let siguientePalindromo = next_pal(numero);
+ 
+ console.log(
+     "El siguiente palíndromo a partir de",
+     numero,
+     "es",
+     siguientePalindromo
+ );
